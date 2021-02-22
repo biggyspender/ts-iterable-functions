0304fe2dcfe8b3052efb58e6f1c75c4444a78835
Chris Sperry <chris.sperry@outthink.io>
2021-02-22 16:23:30 +0000

    fix(intersect): don't create set until iteration

--- src/transformers/intersect.ts
+++ src/transformers/intersect.ts
@@ -1,17 +1,24 @@
 import { _where } from './where'
 import { deferP0 } from 'ts-functional-pipe'
 import { SetFactory } from "../types/SetFactory"
+import { toIterable } from '../helpers/toIterable'
 
 export function _intersect<T>(
   src: Iterable<T>,
   seq: Iterable<T>,
-  setFactory: SetFactory<T> = {createSet:() => new Set()}
+  setFactory: SetFactory<T> = { createSet: () => new Set() }
 ): Iterable<T> {
-  const set: Set<T> = setFactory.createSet()
-  for (const item of seq) {
-    set.add(item)
-  }
-  return _where(src, item => set.has(item))
+  return toIterable(function* () {
+    const set: Set<T> = setFactory.createSet()
+    for (const item of seq) {
+      set.add(item)
+    }
+    for(const item of src){
+      if(set.has(item)){
+        yield item;
+      }
+    }
+  })
 }
 
 export const intersect = deferP0(_intersect)
