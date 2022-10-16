95a702d6759779eea71a77edf4fbfe38c1c860fa
Chris Sperry <chris.sperry@outthink.io>
2022-10-16 18:30:04 +0000

    perf: enable minification and name mangling

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,8 @@
 import path from "path";
 import { build as esbuild, BuildOptions } from "esbuild";
+import { nodeExternalsPlugin } from "esbuild-node-externals";
+
+// import * as fs from "fs";
 
 const baseConfig: BuildOptions = {
   platform: "node",
@@ -7,23 +10,32 @@ const baseConfig: BuildOptions = {
   format: "cjs",
   nodePaths: [path.join(__dirname, "../src")],
   sourcemap: true,
-  external: [],
   bundle: true,
+  minify: true,
+  mangleProps: /_$/,
+  treeShaking: true,
+  plugins: [nodeExternalsPlugin()],
 };
 
 async function main() {
+  // const resultCJS =
   await esbuild({
     ...baseConfig,
     outdir: path.join(__dirname, "../build/cjs"),
     entryPoints: [path.join(__dirname, "../src/index.ts")],
+    // metafile: true,
   });
 
+  // const resultESM =
   await esbuild({
     ...baseConfig,
     format: "esm",
     outdir: path.join(__dirname, "../build/esm"),
     entryPoints: [path.join(__dirname, "../src/index.ts")],
+    // metafile: true,
   });
+  // fs.writeFileSync("metaCJS.json", JSON.stringify(resultCJS.metafile));
+  // fs.writeFileSync("metaESM.json", JSON.stringify(resultESM.metafile));
 }
 
 if (require.main === module) {
