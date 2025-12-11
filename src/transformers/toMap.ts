21276953a7b9223c2cf4530b02f6a08e33783709
Chris Sperry <chris.sperry@outthink.io>
2025-12-11 17:08:13 +0000

    fix: add missing commas in function parameter lists for _toMap and _toSet

--- src/transformers/toMap.ts
+++ src/transformers/toMap.ts
@@ -30,19 +30,19 @@ import getIdentity from "./helpers/getIdentity";
 export function _toMap<T, TKey>(
   src: Iterable<T>,
   keySelector: IndexedSelector<T, TKey>,
-  mapFactory?: MapFactory<TKey>
+  mapFactory?: MapFactory<TKey>,
 ): Map<TKey, T>;
 export function _toMap<T, TKey, TValue>(
   src: Iterable<T>,
   keySelector: IndexedSelector<T, TKey>,
   valueSelector: IndexedSelector<T, TValue>,
-  mapFactory?: MapFactory<TKey>
+  mapFactory?: MapFactory<TKey>,
 ): Map<TKey, TValue>;
 export function _toMap<T, TKey, TValue = T>(
   src: Iterable<T>,
   keySelector: IndexedSelector<T, TKey>,
   valueSelectorOrMapFactory?: IndexedSelector<T, TValue> | MapFactory<TKey>,
-  mapFactoryMaybe?: MapFactory<TKey>
+  mapFactoryMaybe?: MapFactory<TKey>,
 ): Map<TKey, TValue> {
   const vs: IndexedSelector<T, TValue> = (
     !(typeof valueSelectorOrMapFactory === "object")
@@ -71,17 +71,17 @@ export function _toMap<T, TKey, TValue = T>(
  */
 export function toMap<T, TKey>(
   keySelector: IndexedSelector<T, TKey>,
-  mapFactory?: MapFactory<TKey>
+  mapFactory?: MapFactory<TKey>,
 ): (src: Iterable<T>) => Map<TKey, T>;
 export function toMap<T, TKey, TValue>(
   keySelector: IndexedSelector<T, TKey>,
   valueSelector: IndexedSelector<T, TValue>,
-  mapFactory?: MapFactory<TKey>
+  mapFactory?: MapFactory<TKey>,
 ): (src: Iterable<T>) => Map<TKey, TValue>;
 export function toMap<T, TKey, TValue = T>(
   keySelector: IndexedSelector<T, TKey>,
   valueSelectorOrMapFactory?: IndexedSelector<T, TValue> | MapFactory<TKey>,
-  mapFactoryMaybe?: MapFactory<TKey>
+  mapFactoryMaybe?: MapFactory<TKey>,
 ): (src: Iterable<T>) => Map<TKey, TValue> {
   const vs: IndexedSelector<T, TValue> = (
     !(typeof valueSelectorOrMapFactory === "object")
