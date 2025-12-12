a2a063578312d53a6fa99143f7905fe55a383194
Chris Sperry <chris.sperry@outthink.io>
2025-12-12 10:27:54 +0000

    fix: remove comments to trigger new release

--- src/transformers/helpers/OrderedIterable.ts
+++ src/transformers/helpers/OrderedIterable.ts
@@ -24,7 +24,7 @@ export default class OrderedIterable<T> implements Iterable<T> {
             const comp = comparer(aVal, bVal);
             return comp === 0 ? aIdx - bIdx : comp;
           }),
-          map(([value]) => value),
+          map(([value]) => value)
         );
         for (const x of arr) {
           yield x;
@@ -38,16 +38,8 @@ export default class OrderedIterable<T> implements Iterable<T> {
     return [...this[Symbol.iterator]()];
   }
   createNewFrom(
-    b: (builder: ThenComparerBuilder<T>) => ThenComparerBuilder<T>,
+    b: (builder: ThenComparerBuilder<T>) => ThenComparerBuilder<T>
   ) {
     return new OrderedIterable<T>(this.src, b(this.comparerBuilder));
   }
-  // public thenBy<TCmp>(selector: (x: T) => TCmp): OrderedIterable<T> {
-  //   const newBuilder = this.comparerBuilder.thenKey(selector)
-  //   return new OrderedIterable(this.src, newBuilder)
-  // }
-  // public thenByDescending<TCmp>(selector: (x: T) => TCmp): OrderedIterable<T> {
-  //   const newBuilder = this.comparerBuilder.thenKeyDescending(selector)
-  //   return new OrderedIterable(this.src, newBuilder)
-  // }
 }
